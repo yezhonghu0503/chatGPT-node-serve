625fe169eebb7998b168441d4c48c57661e5a20a
wll8 <xwdaysme@gmail.com>
2023-06-09 15:58:51 +0800

    refactor: 清理项目文件

--- .history/router/edits_20230525155125.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import openaiAuth from "../utils/openaiAuth";
-const response = await openaiAuth.createEdit({
-  model: "text-davinci-edit-001",
-  input: "What day of the wek is it?",
-  instruction: "Fix the spelling mistakes",
-});
--- .history/utils/openaiAuth_20230525155004.js
+++
@@ -1,7 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-export default function openaiAuth() {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-}
