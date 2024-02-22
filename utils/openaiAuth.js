beb9be18a3876038faee3130199a3c3ede3de726
yezhonghu0503 <211736726@qq.com>
2024-02-22 15:00:27 +0800

    feat:更新openAI版本

--- utils/openaiAuth.js
+++ utils/openaiAuth.js
@@ -1,11 +1,9 @@
-import { Configuration, OpenAI } from "openai";
+import { OpenAI } from "openai";
 import dotenv from "dotenv";
 
 dotenv.config();
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAI(configuration);
 
 export default openai;
